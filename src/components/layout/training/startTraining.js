b906740e35336fd307accff194c2246dd2cc5ee2
breno-aredes <breno_mg10@hotmail.com>
2023-08-01 01:32:32 -0300

    feat: animation on training.query

--- src/components/layout/training/startTraining.js
+++ src/components/layout/training/startTraining.js
@@ -1,7 +1,13 @@
+import styled from "styled-components";
+
 export function StartTraining() {
   return (
-    <>
+    <Div>
       <h1>nova info</h1>
-    </>
+    </Div>
   );
 }
+
+const Div = styled.div`
+  height: 600px;
+`;
